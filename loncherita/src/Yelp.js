cad559e8facb17b22d82e451b42eefe7259a24d0
ylee1101 <yongho19911101@hotmail.com>
2017-11-09 00:58:02 -0800

    added react-app

---
+++ loncherita/src/Yelp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Yelp.css';
+
+class Yelp extends Component {
+    render() {
+        return (
+            <h1> Hello this is yelp section </h1>
+        )
+    }
+}
+
+export default Yelp;
