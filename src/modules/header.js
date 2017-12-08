620d78b6d3488bf6253149d22ce3b25d682d8c1c
Max S <mschmell@uci.edu>
2017-12-07 19:42:13 -0800

    Login and SignUp are justified to the right

--- src/modules/header.js
+++ src/modules/header.js
@@ -26,24 +26,16 @@ const Header = () => (
           <LinkContainer to="/gallery">
             <NavItem>Gallery</NavItem>
           </LinkContainer>
-          <LinkContainer to="/login">
-            <NavItem>Login</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/signup">
-            <NavItem>Sign Up</NavItem>
-          </LinkContainer>
         </Nav>
           
-          {/*<Nav pullRight>
-            <Navbar.Form className="right-nav">
-            <FormGroup>
-              <InputGroup>
-                <InputGroup.Addon><Glyphicon glyph="search" /></InputGroup.Addon>
-                <FormControl type="text" style={{width:'125px'}} placeholder="Search..." />
-              </InputGroup>
-            </FormGroup>
-            </Navbar.Form>
-          </Nav>*/}
+        <Nav pullRight>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/signup">
+              <NavItem>Sign Up</NavItem>
+            </LinkContainer>
+        </Nav>
 
       </Navbar.Collapse>
     </Navbar>
